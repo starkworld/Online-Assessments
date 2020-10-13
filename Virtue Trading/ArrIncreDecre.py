5fb4db1ac8483e80299d018b5cf0a379b5ac6a7b
stark world <nicky.kalyan438@gmail.com>
2020-10-13 16:35:14 -0400

    Virtu Practice Questions

---
+++ Virtue Trading/ArrIncreDecre.py
@@ -0,0 +1,15 @@
+def solution(A):
+    # write your code in Python 3.6
+    if len(A) == 4:
+        return A
+    k = 0
+    while A != sorted(A):
+        for i in range(1, len(A) - 1):
+            if A[i - 1] > A[i] < A[i + 1]:
+                A[i] = A[i] + 1
+            elif A[i - 1] < A[i] > A[i + 1]:
+                A[i] = A[i] - 1
+    return A
+
+
+print(solution([1, 6, 3, 4, 3, 5]))
