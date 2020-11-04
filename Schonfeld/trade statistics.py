6f17cb9c34af49d5b11812b118597bc1972644d7
stark world <nicky.kalyan438@gmail.com>
2020-11-04 14:37:54 -0500

    Updates

---
+++ Schonfeld/trade statistics.py
@@ -0,0 +1,15 @@
+"""OA practice"""
+
+
+def trades(lis):
+    lst1 = []
+    for i in lis:
+        j = i.split('|')
+        lst1.append(int(j[0]) * int(j[1]))
+    lst1.sort()
+    largest_trades = sum(lst1[-3:])
+    smallest_trades = sum(lst1[:2])
+    return str(largest_trades) + '|' + str(smallest_trades)
+
+
+print(trades(['25100|1107', '24593|1916', '24560|7369', '24974|2120', '24832|3239']))
