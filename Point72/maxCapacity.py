1e239ecdfbf92395ad1bfbb840aea81df7d5b02e
stark world <nicky.kalyan438@gmail.com>
2020-11-10 03:39:39 -0500

    Updates

--- Point72/maxCapacity.py
+++ Point72/maxCapacity.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 import time
 
 start = time.time()
@@ -25,11 +27,34 @@ def maxCapacity(sequence, ma):
     return max([sum(i) for i in k if sum(i) <= ma])
 
 
+def weightCapcity(weights, maxSize):
+    # Creating all sublists and appending into subs
+    subs = []
+    for i in range(0, len(weights) + 1):
+        # Creating sublist using combinations, which is built in function in python
+        temp = [list(x) for x in combinations(weights, i)]
+        if len(temp) > 0:
+            subs.extend(temp)
+    # Intializing maximum with zero value
+    maximum = 0
+    # For each list calculating a value
+    for i in subs:
+        count = 0
+        for j in i:
+            count = count + j
+        # Finding maximum using condition
+        if count > maximum and count <= maxSize:
+            maximum = count
+    # Returning maximum
+    return maximum
+
+
 weights = [7, 10, 19, 37, 30, 11, 35, 16]
 max_Capacity = 49
 # weights = [1, 3, 5]
 # max_Capacity = 7
 print(maxCapacity(weights, max_Capacity))
+print(weightCapcity(weights, max_Capacity))
 
 # time.sleep(1)
 end = time.time()
